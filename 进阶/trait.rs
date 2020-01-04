892e6267156e44b4f67a7588311527874bd7e9ff
mengll <1633094010@qq.com>
2020-01-04 18:45:36 +0800

    Create trait.rs
    特性相关的复习

---
+++ 进阶/trait.rs
@@ -0,0 +1,54 @@
+struct Employee {
+    name :String,
+    company:String,
+    age:u32
+}
+
+impl Employee {
+    fn show(&self){
+        println!("{}",self.company)
+    }
+    fn kill(&mut self,name:String){
+        println!("{}",self.name);
+        self.name = name;
+        println!("{}",self.name)
+    }
+}
+
+// 创建接口计算数据
+trait Gk {
+    fn jj(&self);
+    fn jk(&self){
+        println!("{}","接口方法内部的调用")
+    }
+}
+
+// 执行结构体的操作
+impl Gk for Employee {
+    // 数据当前对象的年龄
+    fn jj(&self){
+        println!("{}",self.age)
+    }
+}
+
+// 函数返回结构体
+fn new_struct() ->Employee{
+    Employee{
+        name:String::from("name"),
+        company:String::from("two"),
+        age:12
+    }
+}
+
+fn main(){
+    let instance_name = Employee{name:String::from("name"),company:String::from("asd"),age:1};
+    println!("{}",instance_name.age);
+    instance_name.show();
+    instance_name.jj();
+    instance_name.jk();
+    
+    let change_name:&mut Employee = &mut Employee{..instance_name};
+    change_name.kill("fu".to_string());
+    let cc =  &mut new_struct();
+    cc.kill("fj".to_string());
+}
