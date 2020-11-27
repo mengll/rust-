ef601d31aaca5ed0d4a3765ac2a310a15e74fc5b
mengll <1633094010@qq.com>
2020-11-27 10:27:17 +0800

    Update mongodb.rs

--- 基础/db/mongodb.rs
+++ 基础/db/mongodb.rs
@@ -31,3 +31,45 @@ fn main(){
   }
 
 }
+
+
+// 新版的MongoDB的读取的操作 mongodb 1.1.1 需要的MongoDB的数据库的集群的需要 4.2 以上才行
+
+use mongodb::{Client, options::{ClientOptions,FindOptions}};
+use mongodb::bson::doc;
+use tokio::stream::StreamExt;
+use mongodb::{
+    bson::Document
+};
+
+#[tokio::main]
+async fn main(){
+    let client_options = ClientOptions::parse("mongodb://root:passs@192.168.1.241:44126/admin").await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    let db = client.database("dsp");
+    let adtconf = db.collection("adt_conf");
+    let limit  = FindOptions::builder().limit(1).build();
+    let mut res  = adtconf.find(doc!{"test":1},limit).await.unwrap();
+    while  let Some(result) =  res.next().await {
+        match result {
+             Ok(docs)=> {
+                 println!("dock");
+                 let d = docs as Document;
+                 let s = d.get("test").unwrap().as_i32().unwrap();
+                 println!("{}",s)},
+             Err(e)=> println!("e：a {:?}",e.labels())
+        }
+    }
+
+    // match res {
+    //     Some(docuemnt) => for k in  docuemnt.iter(){
+    //         println!("{} ---{}",k.0,k.1);
+    //     } ,
+    //     None => return
+    // }
+    // for db_name in db.list_collection_names(None).await.unwrap() {
+    //     println!("{}", db_name);
+    // }
+
+}
+
